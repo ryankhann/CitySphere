701636d1da7e5c1ed61b38cfdfd3d8ffb0faf6e9
ryankhann <ryankhann028@gmail.com>
2026-02-04 18:11:04 +0600

    Update login API URL to use API_URL variable

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch(`${API_URL}/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
