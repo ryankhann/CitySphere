b02272af739050d80e7c982bb94d848390fc58e6
ryankhann <ryankhann028@gmail.com>
2026-02-04 18:06:16 +0600

    Import API_URL in Login component

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { API_URL } from '../api';
 
 const Login = () => {
   const [email, setEmail] = useState('');
